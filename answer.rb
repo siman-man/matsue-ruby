122293fc0b87a0e96590c864a13b21b7bee376da
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-09-05 01:32:12 +0900

    143

--- answer.rb
+++ answer.rb
@@ -1,9 +1,9 @@
-n=gets
+gets
 h={}
-$stdin.read.split(?\n).sort.map{|s|
+$<.read.split(?\n).sort.map{|s|
 r=s.reverse
 r==s&&$\||=s
-h[r]?($*<<r;h[r]=nil):(h[s]=1)
+h[r]?$*<<r&&h[r]=$a:h[s]=1
 }
 f=$*.sort*''
 puts [f,$\,f.reverse]*''

05582879d7aa85d6ed26605f147bbef6021a8706
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-09-05 01:52:35 +0900

    137

--- answer.rb
+++ answer.rb
@@ -1,9 +1,8 @@
 gets
 h={}
-v=$<.read.split(?\n).sort.map{|s|
+f=$<.read.split(?\n).sort.map{|s|
 r=s.reverse
 r==s&&$\||=s
 h[r]?(h[r]=$a;r):h[s]=''
-}
-f=v.sort*''
+}.sort*''
 puts [f,$\,f.reverse]*''

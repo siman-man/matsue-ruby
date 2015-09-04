85996b2bfe792a79fd0f7f72f26775ac8a37e932
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-09-04 23:08:19 +0900

    temp save

--- answer.rb
+++ answer.rb
@@ -2,5 +2,5 @@ n=gets.to_i
 wc=Hash.new(0)
 n.times{s=gets.chop;wc[[s,s.reverse].min]+=1}
 f=wc.select{|k,v|v.even?}.sort.map{|e|e[0]*(e[1]/2)}.join
-m=wc.select{|k,v|v.odd?&&k==k.reverse}.keys.sort[0]
+m=wc.select{|k,v|v.odd?&&k==k.reverse}.keys.sort.join
 puts [f,m,f.reverse].join

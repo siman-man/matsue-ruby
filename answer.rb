efe52756264743f76d45dcc27217eee4c72095c3
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-09-07 06:33:41 +0900

    96

--- answer.rb
+++ answer.rb
@@ -1,6 +1,7 @@
-f=$<.read.split.sort
-f[0]=n=m=''
-f.map{|s|
-f.index(r=s.reverse)&&(s<r&&n+=s;s>r||m=r+m)
+gets
+f=$<.read.split
+n=m=''
+f.sort.map{|s|
+f-[r=s.reverse]!=f&&(s<r&&n+=s;s>r||m=r+m)
 }
 $><<n+m

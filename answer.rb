a386e3214c0c2882df8df4a213222389fa503fa9
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-09-08 21:44:49 +0900

    94 correct

--- answer.rb
+++ answer.rb
@@ -1,6 +1,5 @@
-n,*f=$<.read.split.sort
-n=m=''
-f.map{|s|
-f-[r=s.reverse]!=f&&(s<r&&n+=s;s>r||m=r+m)
-}
-$><<n+m
+t,*f=$<.read.split.sort
+m=''
+$><<f.map{|s|
+f.index(r=s.reverse)&&(r<s||m=r+m;s<r)?s:''
+}*''+m
